7bffd389c16a45cc42dbc44cd03e1287e0a189a1
Jonathan Dust <d24dust@hotmail.com>
2018-05-05 18:58:02 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -16,33 +16,33 @@ class Owner
 
   def buy_fish(name)
     fish = Fish.new(name)
-    @pets[fishes] << fish
+    @pets[:fishes] << fish
   end
 
   def buy_cat(name)
     cat = Cat.new(name)
-    @pets[cats] << cat
+    @pets[:cats] << cat
   end
 
   def buy_dog(name)
     dog = Dog.new(name)
-    @pets[dogs] << dog
+    @pets[:dogs] << dog
   end
 
   def walk_dogs
-    @pets[dogs].each do |dog|
+    @pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    @pets[cats].each do |cat|
+    @pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
 
   def feed_fish
-    @pets[fishes].each do |fish|
+    @pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
   end
